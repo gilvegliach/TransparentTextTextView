1f5ab3b6e9db1fc873c3cbe2be4628ee9017ab2b
Gil Vegliach <gil.vegliach@gmail.com>
2015-08-30 20:42:31 +0200

    First commit

---
+++ app/src/main/java/it/gilvegliach/blog/seethrough/MainActivity.java
@@ -0,0 +1,116 @@
+package it.gilvegliach.blog.seethrough;
+
+import android.annotation.SuppressLint;
+
+import android.content.Context;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+
+import android.graphics.PorterDuff.Mode;
+
+import android.graphics.PorterDuffXfermode;
+
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+
+import android.os.Bundle;
+
+import android.support.v7.app.AppCompatActivity;
+
+import android.util.AttributeSet;
+
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public static final class SeeThroughTextView extends TextView {
+        Bitmap mMaskBitmap;
+        Canvas mMaskCanvas;
+        Paint mPaint;
+        Drawable mBackground;
+        Bitmap mBackgroundBitmap;
+        Canvas mBackgroundCanvas;
+        boolean mSetBoundsOnSizeAvailable;
+
+        public SeeThroughTextView(final Context context) {
+            super(context);
+            init();
+        }
+
+        public SeeThroughTextView(final Context context, final AttributeSet attrs) {
+            super(context, attrs);
+            init();
+        }
+
+        private void init() {
+            mPaint = new Paint();
+            mPaint.setXfermode(new PorterDuffXfermode(Mode.DST_OUT));
+            super.setTextColor(Color.BLACK);
+            super.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        }
+
+        @Override
+        @Deprecated
+        public void setBackgroundDrawable(final Drawable bg) {
+            mBackground = bg;
+
+            int w = getWidth();
+            int h = getHeight();
+
+            // Layout has not run
+            if (w == 0 || h == 0) {
+                mSetBoundsOnSizeAvailable = true;
+                return;
+            }
+
+            mBackground.setBounds(0, 0, w, h);
+            invalidate();
+        }
+
+        @Override
+        public void setBackgroundColor(final int color) {
+            setBackgroundDrawable(new ColorDrawable(color));
+        }
+
+        @Override
+        protected void onSizeChanged(final int w, final int h, final int oldw, final int oldh) {
+            super.onSizeChanged(w, h, oldw, oldh);
+            mBackgroundBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+            mBackgroundCanvas = new Canvas(mBackgroundBitmap);
+            mMaskBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+            mMaskCanvas = new Canvas(mMaskBitmap);
+
+            if (mSetBoundsOnSizeAvailable) {
+                mBackground.setBounds(0, 0, w, h);
+                mSetBoundsOnSizeAvailable = false;
+            }
+        }
+
+        @Override
+        protected void onDraw(final Canvas canvas) {
+            drawMask();
+            drawBackground();
+            canvas.drawBitmap(mBackgroundBitmap, 0.f, 0.f, null);
+        }
+
+        private void drawBackground() {
+            mBackground.draw(mBackgroundCanvas);
+            mBackgroundCanvas.drawBitmap(mMaskBitmap, 0.f, 0.f, mPaint);
+        }
+
+        // draw() calls onDraw() leading to stack overflow
+        @SuppressLint("WrongCall")
+        private void drawMask() {
+            mMaskCanvas.drawColor(Color.BLACK, Mode.CLEAR);
+            super.onDraw(mMaskCanvas);
+        }
+    }
+}
