6e1830ca97065338a9b5421b259525cd8562d0bd
Gil Vegliach <gil.vegliach@gmail.com>
2016-01-24 18:50:12 +0100

    Split project in library and sample

---
+++ app/src/main/java/it/gilvegliach/transparenttexttextview/sample/MainActivity.java
@@ -0,0 +1,13 @@
+package it.gilvegliach.transparenttexttextview.sample;
+
+import android.os.Bundle;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
